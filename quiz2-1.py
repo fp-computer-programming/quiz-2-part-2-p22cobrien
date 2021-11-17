51a6578f53eb54b87d4e8397d4f511525e3f3251
p22cobrien <p22cobrien@fairfieldprep.org>
2021-11-17 10:10:59 -0500

    submit quiz2-1.py

---
+++ quiz2-1.py
@@ -0,0 +1,20 @@
+# Author: CMOB 11/17/2021
+
+day = input("What letter day is it? ")
+
+if day == "a" or "A":
+    print("You have class because it is day A.")
+elif day == "b" or "B":
+    print("You have class because it is day B.")
+elif day == "c" or "C":
+    print("You have class because it is day C.")
+elif day == "d" or "D":
+    print("You have class because it is day D.")
+elif day == "e" or "E":
+    print("You have class because it is day E.")
+elif day == "f" or "F":
+    print("You have class because it is day F.")
+elif day == "g" or "G":
+    print("You have class because it is day G.")
+else:
+    print("No suitable letter for day inputed.")
